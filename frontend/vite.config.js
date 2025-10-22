6937ead524962acfd5ac4a35993d2812aa913798
Keith Harvey <keith.harvey@timeout.com>
2025-10-22 17:03:01 +0100

    use pwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
 	  react(),
 	  tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'vite.svg'],
+      manifest: {
+        name: 'Budgeter',
+        short_name: 'Budgeter',
+        description: 'A simple budgeting app.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'vite.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: 'vite.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
   ],
   server: {
     allowedHosts: [
